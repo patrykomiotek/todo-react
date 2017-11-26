f2b0a8a02552d3c62db25de783bde0776cb7b20b
Patryk Omiotek <patryk.omiotek@gmail.com>
2017-11-26 11:54:30 +0100

    examples of controlled and uncontrolled components

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import {BrowserRouter as Router, Route } from 'react-router-dom';
 import Dashboard from './components/Dashboard';
 import Header from './components/Header';
 import ToDo from './components/ToDo';
+import Form from './components/form'
 import './App.css';
 
 class App extends Component {
@@ -15,6 +16,7 @@ class App extends Component {
           <Route exact path='/' component={Dashboard}/>
           <Route path='/todo' component={ToDo}/>
           <Route path='/counter' component={null}/>
+          <Route path='/form' component={Form}/>
         </div>
       </Router>
     );
